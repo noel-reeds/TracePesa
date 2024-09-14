ab820bb1981b36ac92f5e75b370d7ff2a3bf8ff1
altoid <noelreeds01@gmail.com>
2024-09-14 23:46:29 +0000

    defines a user

--- models/user.py
+++ models/user.py
@@ -1,28 +1,9 @@
-from flask_login import UserMixin
-from models.base import Base
+class User():
+    """User model, defines a user and attrs"""
 
-
-class User(UserMixin, db.Model):
-    """contains users of the API"""
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    income = db.Column(db.Integer, nullable=False)
-
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    """adds user to users db"""
-    user = request.get_json()
-    existing_emails = User.query.order_by(User.email).all()
-    if user['email'] in existing_emails:
-        return 'user already exists'
-    new_user = User(username=user['username'], name=user['name'],
-                email=['email'], income=user['income'])
-    db.session.add(new_user)
-    db.session.commit()
-    return 'Registration successful'
-
-
-@app.route('/delete_user', methods=['POST'])
+    def __init__(self, *args, **kwargs):
+        """initializes a user object"""
+        self.first_name = kwargs.get('first_name')
+        self.last_name = kwargs.get('last_name')
+        self._password = kwargs.get('_password')
+        self.monthly_income = kwargs.get('monthly_income')
