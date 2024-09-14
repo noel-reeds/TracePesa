1b42c6d55960644e6ab7e46bd5ff3c5e42633bff
altoid <noelreeds01@gmail.com>
2024-09-14 11:51:04 +0000

    added user set up in database

--- api/user.py
+++ api/user.py
@@ -6,3 +6,28 @@ import bicrypt
 class User(Base):
     """class to define users"""
     pass
+
+class User(db.Model):
+    """contains users of the API"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    income = db.Column(db.Integer, nullable=False)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """adds user to users db"""
+    user = request.get_json()
+    existing_emails = User.query.order_by(User.email).all()
+    if user['email'] in existing_emails:
+        return 'user already exists'
+    new_user = User(username=user['username'], name=user['name'],
+                email=['email'], income=user['income'])
+    db.session.add(new_user)
+    db.session.commit()
+    return 'Registration successful'
+
+
+@app.route('/delete_user', methods=['POST'])
