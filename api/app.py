3acc4ed4468919f8d79c5459d6679b93856590ec
altoid <noelreeds01@gmail.com>
2024-09-14 12:30:39 +0000

    added cehck for existing expenditure

--- api/app.py
+++ api/app.py
@@ -1,13 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
+from . import db # presumably from init..
 
 
-class Expenses(db.Model):
+class Expense(db.Model):
     """attributes of the expenses"""
     id = db.Column(db.Integer, foreign_key=True)
     category = db.Column(db.String(50), nullable=False)
@@ -18,36 +15,16 @@ class Expenses(db.Model):
         return "<Expense added to %r>" % self.category
 
 
-class User(db.Model):
-    """contains users of the API"""
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    income = db.Column(db.Integer, nullable=False)
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    """adds user to users db"""
-    user = request.get_json()
-    existing_emails = User.query.order_by(User.email).all()
-    if user['email'] in existing_emails:
-        return 'user already exists'
-    new_user = User(username=user['username'], name=user['name'],
-                email=['email'], income=user['income'])
-    db.session.add(new_user)
-    db.session.commit()
-    return 'Registration successful'
-
-
 @app.route('/add_expense', methods=['POST'])
 def add_expenses():
     """adds new expenses to the database"""
     expense_data = request.get_json()
     amount = expense_data['amount']
     category = expense_data['category']
-    new_expense = Expenses(username=username,
-                category=category, name=name, amount=amount)
+    name = expense_data['name']
+    new_expense = Expenses(category=category, name=name, amount=amount)
+    if new_expense.name in Expense.query.order_by(Expense.name).all():
+        return 'expenditure already exists'
     db.session.add(new_expense)
     db.session.commit()
     return 'New expense added'
