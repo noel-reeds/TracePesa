5e17ecaa0c77c7eeaf75359881fe13463feb64b5
altoid <noelreeds01@gmail.com>
2024-09-16 14:59:03 +0000

    restructured attrs

--- models/expense.py
+++ models/expense.py
@@ -4,8 +4,13 @@ from datetime import datetime
 
 class Expense(db.Model):
     """Entails details of an expenditure"""
-    user_id = db.Column(db.Integer, foreign_key=True)
-    expense_id = db.Column(db.Integer, primary_key=True)
+
+    __tablename__ = 'expenses'
+
+    id = db.Column(db.Integer(50), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Integer(50), nullable=False)
     date = db.Column(db.datetime, nullable=False)
