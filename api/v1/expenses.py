760c04c28302b5c5a82cd436560bc460d12a4553
altoid <noelreeds01@gmail.com>
2024-09-16 15:07:50 +0000

    added endpoont to add an expenditure

--- api/v1/expenses.py
+++ api/v1/expenses.py
@@ -1,6 +1,28 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request, datetime, jsonify
 from models.expense import Expense
+from app import db
 
 expense = Blueprint('expense', __name__)
 
-@expense.route(
+@expense.route('expense/add', methods=['POST'])
+def add_expense():
+    """Adds an expenditure to the database"""
+    expense_info = request.get_json()
+    user_id = expense_info.get('user_id')
+    category = expense_info.get('category')
+    desc = expense_info.get('desc')
+    name = expense.info.get('name')
+    amount = expense_info.get('amount')
+    date = datetime.strptime(expense_info.get('date'), '%Y-%m-%d')
+
+    new_expense = Expense(user_id=user_id, category=category, desc=desc,
+            name=name, amount=amount, date=date)
+    db.session.add(new_expense)
+    db.session.commit()
+    return jsonify({'message': 'Expense added successfully'})
+
+
+@expense.route('expense/remove/int:expense_id>', methods=['POST'])
+def remove_expense(expense_id):
+    """Deletes an expense from database"""
+    expense = 
