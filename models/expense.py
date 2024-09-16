3ca0ff3af29293421b06571df31acbfe35cf9573
altoid <noelreeds01@gmail.com>
2024-09-16 13:28:38 +0000

    changed expense attrs name

--- models/expense.py
+++ models/expense.py
@@ -8,4 +8,4 @@ class Expense(db.Model):
     expense_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Integer(50), nullable=False)
-    created = db.Column(db.datetime, nullable=False)
+    date = db.Column(db.datetime, nullable=False)
