57ed3dbe40252fa19e727a5a47da9c25abbb5702
altoid <noelreeds01@gmail.com>
2024-09-24 22:37:44 +0000

    correct configs

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from models.user import User
 
 
 db = SQLAlchemy()
@@ -16,12 +15,13 @@ def fintrack_app():
     login_manager = LoginManager()
     login_manager.login_view ='auth.login'
     login_manager.init_app(app)
-    
+
+    from models.user import User
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
+    from api.v1.auth import auth as auth_blueprint
     from .main import main as main_blueprint
     from models.user import expenditure as expenditure
 
@@ -33,4 +33,4 @@ def fintrack_app():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    fintrack_app()
