41ca0ef211f5e610b8e32164b8bbc92ed835f4a9
altoid <noelreeds01@gmail.com>
2024-09-27 12:32:24 +0000

    user_id to point to user table correctly

--- models/expense.py
+++ models/expense.py
@@ -1,4 +1,4 @@
-from models import db
+from . import db
 from datetime import datetime
 
 
@@ -8,7 +8,7 @@ class Expense(db.Model):
     __tablename__ = 'expenses'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(50), nullable=False)
