586202ed34845fdced4f4c3f273ce5d05d327c47
altoid <noelreeds01@gmail.com>
2024-09-27 12:29:26 +0000

    datetime correctly formatted

--- api/v1/expenses.py
+++ api/v1/expenses.py
@@ -1,19 +1,19 @@
-from flask import Flask, Blueprint, request, datetime, jsonify
+from flask import Flask, Blueprint, request, jsonify
 from models.expense import Expense
 from app import db
+from datetime import datetime
 
 expense = Blueprint('expense', __name__)
 
-@expense.route('expense/add', methods=['POST'])
+@expense.route('/api/v1/expense/add', methods=['POST'])
 def add_expense():
     """Adds an expenditure to the database"""
-    expense_info = request.get_json()
-    user_id = expense_info.get('user_id')
+    expense_info = request.json
     category = expense_info.get('category')
     desc = expense_info.get('desc')
-    name = expense.info.get('name')
+    name = expense_info.get('name')
     amount = expense_info.get('amount')
-    date = datetime.strptime(expense_info.get('date'), '%Y-%m-%d')
+    date = datetime.strptime(datetime.utcnow(), '%Y-%m-%d')
 
     new_expense = Expense(user_id=user_id, category=category, desc=desc,
             name=name, amount=amount, date=date)
@@ -22,7 +22,7 @@ def add_expense():
     return jsonify({'message': 'Expense added successfully'})
 
 
-@expense.route('expense/remove/int:expense_id>', methods=['POST'])
+@expense.route('/expense/remove/int:expense_id>', methods=['POST'])
 def remove_expense(expense_id):
     """Deletes an expense from database"""
     expense = Expense.query.get_or_404(expense_id)
@@ -35,7 +35,7 @@ def remove_expense(expense_id):
     except:
         return jsonify({'message': 'Expense does not exist'})
 
-@expense.route('expense/get_expenses/<int:user_id>', methods=['GET'])
+@expense.route('/expense/get_expenses/<int:user_id>', methods=['GET'])
 def get_expenses(user_id):
     """returns all expenses of a user"""
     expenses = Expense.query.filter_by(user_id).all()
@@ -47,7 +47,7 @@ def get_expenses(user_id):
         return jsonify({'message': 'No expenses for the user, add some!'})
 
 
-@expense.route('expense/update/<int:expense_id>', methods=['PUT'])
+@expense.route('/expense/update/<int:expense_id>', methods=['PUT'])
 def update_expense(expense_id):
     """Updates a user expenditure"""
     expense = Expense.query.get_or_404(expense_id)
