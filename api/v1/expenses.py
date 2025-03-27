ecfc0f4dec8026b03e5ce9cd750cf005c98a60e8
altoids <noelreeds01@gmail.com>
2025-03-27 15:49:47 +0300

    retrieves and persists expenses

--- api/v1/expenses.py
+++ api/v1/expenses.py
@@ -9,19 +9,18 @@ expense = Blueprint('expense', __name__)
 @expense.route('/api/v1/expense/add/<int:user_id>', methods=['POST'])
 def add_expense(user_id):
     """Adds an expenditure to the database"""
-    expense_info = request.json
-    category = expense_info.get('category')
-    desc = expense_info.get('desc')
-    name = expense_info.get('name')
-    amount = expense_info.get('amount')
-    current_date = date.today()
+    response = request.json
+    category = response.get('category')
+    desc = response.get('desc')
+    name = response.get('name')
+    amount = response.get('amount')
     
     # check if user exists before expense persists
     user = User.query.filter_by(id=user_id).first()
     if not user:
         return jsonify({'message': 'user does not exist'})
-    new_expense = Expense(user_id=user_id,category=category, desc=desc,
-            name=name, amount=amount, date=current_date)
+    new_expense = Expense(user_id=user_id,category=category,\
+                        desc=desc,name=name,amount=amount)
     db.session.add(new_expense)
     db.session.commit()
     return jsonify({'message': 'expense add success'})
@@ -43,7 +42,7 @@ def remove_expense(expense_id):
 
 @expense.route('/api/v1/expenses/<int:user_id>', methods=['GET'])
 def expenses(user_id):
-    """returns all expenses of a user"""
+    """Returns all expenses of a user"""
     if not User.query.filter_by(id=user_id).first():
         return jsonify({'message': 'user does not exist'})
     expenses = Expense.query.filter_by(user_id=user_id).all()
@@ -61,8 +60,20 @@ def update_expense(expense_id):
         expense.amount = expense_info.get('amount')
         expense.name = expense_info.get('name')
         expense.desc = expense_info.get('desc')
-        expense.date = datetime.strptime(expense_info.get('date'), '%Y-%m-%d')
+        expense.date = date.today()
 
         db.session.commit()
     except:
         return jsonify({'message': 'Error occured updating expenditure'})
+
+
+@expense.route('/api/v1/expense/<expense_id>', methods=['GET'])
+def ret_expense(expense_id):
+    """Return a specific expenditure"""
+    expense = Expense.query.filter_by(id=expense_id).first()
+    if expense is None:
+        return jsonify({"message": "expenditure does not exists"})
+    try:
+        return jsonify(expense.to_dict())
+    except Exception as err:
+        return jsonify({"error": "{}".format(err)})
--- models/expense.py
+++ models/expense.py
@@ -1,5 +1,7 @@
-from . import db
-from datetime import datetime
+import uuid
+from models import db
+from datetime import datetime, date
+from flask import jsonify as js
 
 
 class Expense(db.Model):
@@ -7,13 +9,29 @@ class Expense(db.Model):
 
     __tablename__ = 'expenses'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+
+    def __init__(self, *args, **kwargs):
+        """Initializes expenses' ids"""
+        self.id = str(uuid.uuid4())
+        self.date = date.today()
+        # check kwargs supplied for instance creation
+        if kwargs:
+            for key, value in kwargs.items():
+                if value is None:
+                    return js({"message": "attr {} cannot be None".format(key)})
+        self.amount = kwargs.get('amount')
+        self.category = kwargs.get('category')
+        self.name = kwargs.get('name')
+        self.desc = kwargs.get('desc')
+        self.user_id = kwargs.get('user_id')
     
     def to_dict(self):
         """Returns a dictionary of the expense"""
